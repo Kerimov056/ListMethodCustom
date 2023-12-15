695fc7f5a6ae15ca2756825f9cc4523d21223d49
Kerimov056 <ulvisk@code.edu.az>
2023-12-15 18:07:32 +0400

    Home Page

---
+++ tasklartelrrrrr/Entities/Test.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace tasklartelrrrrr.Entities;
+
+public class Test<T>
+{
+    private T[] _arr;
+
+    public Test()
+    {
+        _arr = new T[0];
+    }
+
+    public void Add(T obj)
+    {
+        Array.Resize(ref _arr, _arr.Length + 1);
+        _arr[_arr.Length - 1] = obj;
+    }
+
+    public void ShowAll()
+    {
+        foreach (var item in _arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void Clear()
+    {
+        Array.Resize(ref _arr, 0);
+    }
+
+
+    public void CustomAddRange(IEnumerable<T> list)
+    {
+        foreach (var item in list)
+        {
+            Add(item);
+        }
+    }
+
+    public bool customContains(T obj)
+    {
+        foreach (var item in _arr)
+        {
+            if (Equals(item, obj)) return true;
+        }
+        return false;
+    }
+
+    public int customIndexOf(T obj)
+    {
+        for (int i = 0; i < _arr.Length; i++)
+        {
+            if (Equals(_arr[i], obj)) return i;
+        }
+        return -1;
+    }
+
+
+    public void customRemove(T obj)  // meselen arrayde 3 dene Ulvi varsa hamisin silir 
+    {
+        for (int i = 0; i <= _arr.Length - 1; i++)
+        {
+            if (Equals(_arr[i], obj))
+            {
+                _arr[i] = default;  
+                for (int j = i; j < _arr.Length - 1; j++)
+                {
+                    _arr[j] = _arr[j + 1];
+                }
+                Array.Resize(ref _arr, _arr.Length - 1);
+            }
+        }
+    }
+
+}
---
+++ tasklartelrrrrr/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using tasklartelrrrrr.Entities;
+
+Test<string> test = new Test<string>();
+test.Add("Salam1");
+test.Add("Salam");
+test.Add("Salam1");
+test.Add("Salam");
+test.Add("Salam");
+test.customRemove("Salam");
+test.ShowAll();
+
+
+
+
+
+
+
+
+//(int, int) Swap(int a, int b)
+//{
+//    return (b, a);
+//}
+//Console.WriteLine(Swap(2,3));
+
